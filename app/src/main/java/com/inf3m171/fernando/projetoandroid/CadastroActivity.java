892636456eeb69a65e0935979a6d18f0f5cf1507
Fernando Mottin Maschmann <fernando.maschmann@outlook.com>
2018-09-25 11:53:37 -0300

    Todas telas feitas e em progresso com os arquivos java

--- app/src/main/java/com/inf3m171/fernando/projetoandroid/CadastroActivity.java
+++ app/src/main/java/com/inf3m171/fernando/projetoandroid/CadastroActivity.java
@@ -1,13 +1,85 @@
 package com.inf3m171.fernando.projetoandroid;
 
+import android.support.annotation.NonNull;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class CadastroActivity extends AppCompatActivity {
+
+    private EditText etNome, etEmail, etSenha, etConfirmaSenha;
+    private Button btnCadastrar;
+
+    private FirebaseAuth autenticacao;
+    private FirebaseDatabase database;
+    private DatabaseReference reference;
+    private String erro = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastro);
+
+        etNome = (EditText) findViewById(R.id.etNome);
+        etEmail = (EditText) findViewById(R.id.etEmail);
+        etSenha = (EditText) findViewById(R.id.etSenha);
+        etConfirmaSenha = (EditText) findViewById(R.id.etConfirmaSenha);
+
+        btnCadastrar = (Button) findViewById(R.id.btnCadastrar);
+
+        btnCadastrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                criarUsuario();
+            }
+        });
+    }
+
+    private void criarUsuario(){
+        final String nome = etNome.getText().toString();
+        final String senha = etSenha.getText().toString();
+        String confirma = etConfirmaSenha.getText().toString();
+        final String email = etEmail.getText().toString();
+
+        if (!nome.isEmpty() && !senha.isEmpty() && senha.equals(confirma) && !email.isEmpty()){
+            autenticacao = FirebaseAuth.getInstance();
+            autenticacao.createUserWithEmailAndPassword(email, senha).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if (task.isSuccessful()){
+                        database = FirebaseDatabase.getInstance();
+                        String id = autenticacao.getCurrentUser().getUid();
+                        reference = database.getReference("usuarios").child(id);
+                        reference.child("nome").setValue(nome);
+                        reference.child("email").setValue(email);
+                        reference.child("senha").setValue(senha);
+                    }else {
+                        erro = "Não foi possivel criar o usuário!";
+                    }
+                }
+            });
+        }else {
+            erro = "O campo e-mail deve ser preenchido e os campos de senha devem ser iguais";
+        }
+
+        if (!erro.isEmpty()){
+            AlertDialog.Builder alerta = new AlertDialog.Builder(this);
+            alerta.setTitle("Atenção");
+            alerta.setMessage(erro);
+            alerta.setNeutralButton("OK", null);
+            alerta.show();
+        }else {
+            finish();
+        }
     }
 }
--- app/src/main/java/com/inf3m171/fernando/projetoandroid/LoginActivity.java
+++ app/src/main/java/com/inf3m171/fernando/projetoandroid/LoginActivity.java
@@ -1,15 +1,27 @@
 package com.inf3m171.fernando.projetoandroid;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
 
-    EditText etEmailEntrar, etSenhaEntrar;
-    Button btnCadastrarNovo, btnEntrar;
+    private EditText etEmailEntrar, etSenhaEntrar;
+    private Button btnCadastrarNovo, btnEntrar;
+
+    private FirebaseAuth autenticacao;
+    private FirebaseAuth.AuthStateListener stateListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,18 +33,67 @@ public class LoginActivity extends AppCompatActivity {
         btnCadastrarNovo = (Button) findViewById(R.id.btnCadastrarNovo);
         btnEntrar = (Button) findViewById(R.id.btnEntrar);
 
+        autenticacao = FirebaseAuth.getInstance();
+
         btnCadastrarNovo.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                Intent i = new Intent(LoginActivity.this, CadastroActivity.class);
+                startActivity(i);
             }
         });
 
         btnEntrar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                entrar();
             }
         });
+
+        stateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null){
+                    Intent i = new Intent(LoginActivity.this, ListaActivity.class);
+                    startActivity(i);
+                }
+            }
+        };
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        autenticacao.addAuthStateListener(stateListener);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (stateListener != null){
+            autenticacao.removeAuthStateListener(stateListener);
+        }
+    }
+
+    private void entrar(){
+        String email = etEmailEntrar.getText().toString();
+        String senha = etSenhaEntrar.getText().toString();
+
+        if (email.isEmpty())
+            Toast.makeText(LoginActivity.this, "E-mail deve ser preenchido!", Toast.LENGTH_LONG).show();
+
+        if (senha.isEmpty())
+            Toast.makeText(LoginActivity.this, "E-mail deve ser preenchido!", Toast.LENGTH_LONG).show();
+
+        if (!email.isEmpty() && !senha.isEmpty()){
+            autenticacao.signInWithEmailAndPassword(email, senha).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if (!task.isSuccessful())
+                        Toast.makeText(LoginActivity.this, "Usuario ou senha inválidos!", Toast.LENGTH_LONG);
+                }
+            });
+        }
     }
 }
